a11e5f1c303a11cfb0867f7434af2f281de8173a
Eric Syme <spotlightphotoseattle@gmail.com>
2019-07-14 22:36:36 -0700

    fixed server and activity indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,9 @@ import { Text, ScrollView, FlatList  } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+
+
 
 const mapStateToProps = state => {
     return {
@@ -32,6 +35,7 @@ class About extends Component {
     };
 
     render(){
+        
 
         const renderLeader = ({item, index}) => {
 
@@ -45,18 +49,43 @@ class About extends Component {
             );
         };
 
-        return(
-            <ScrollView>
-                <History />
-                <Card title='Corporate Leadership'>
-                <FlatList 
-                    data={this.props.leaders.leaders}
-                    renderItem={renderLeader}
-                    keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
-            </ScrollView>
-        );
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                    <FlatList 
+                        data={this.props.leaders.leaders}
+                        renderItem={renderLeader}
+                        keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
+                </ScrollView>
+            );
+        }
     }
 }
 
