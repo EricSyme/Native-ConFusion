7c8ae7ee83af895d6ee364a45fcba0774dee6310
Eric Syme <spotlightphotoseattle@gmail.com>
2019-07-28 19:57:24 -0700

    social sharing

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -62,7 +62,17 @@ function RenderDish(props) {
 
             return true;
         }
-    })
+    });
+
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    }
 
     if (dish != null) {
         return(
@@ -92,6 +102,14 @@ function RenderDish(props) {
                         color='#512DA8'
                         onPress={() => props.onShowModal()}
                         />
+                    <Icon style={styles.cardItem}
+                        raised
+                        reverse
+                        name='share'
+                        type='font-awesome'
+                        color='#51D2A8'
+                        style={styles.cardItem}
+                        onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
                 </View>
                 </Card>
             </Animatable.View>
